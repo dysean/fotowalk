04b8ee55cc98bc27746f025e108d5081fbe3ffde
dysean <sean@dy-family.com>
2015-03-05 11:31:30 -0800

    Initial Commit
    yay for boiler plates

---
+++ fotowalk/fotowalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fotowalk
+//
+//  Created by Sean Dy on 3/5/15.
+//  Copyright (c) 2015 fotowalkers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fotowalk/fotowalk/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  fotowalk
+//
+//  Created by Sean Dy on 3/5/15.
+//  Copyright (c) 2015 fotowalkers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
