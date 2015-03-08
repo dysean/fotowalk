f965d7f3857a73d20890141c48dfddfecf915eef
dysean <sean@dy-family.com>
2015-03-08 15:05:33 -0700

    Adding Main Table View Cell
    Adding view cell for the walks and the table in the main view

---
+++ fotowalk/fotowalk/PhotoWalkTableViewCell.m
@@ -0,0 +1,48 @@
+//
+//  PhotoWalkTableViewCell.m
+//  fotowalk
+//
+//  Created by Sean Dy on 3/8/15.
+//  Copyright (c) 2015 fotowalkers. All rights reserved.
+//
+
+#import "PhotoWalkTableViewCell.h"
+#import "UIImageView+AFNetworking.h"
+#import "Location.h"
+#import "Media.h"
+
+@interface PhotoWalkTableViewCell()
+@property (weak, nonatomic) IBOutlet UILabel *walkNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *walkDistanceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *walkTimeLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *walkImageView;
+
+@end
+
+@implementation PhotoWalkTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+    self.backgroundColor = [UIColor blackColor];
+
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (void)setPhotoWalk:(PhotoWalk *)photoWalk {
+    _photoWalk = photoWalk;
+    
+    PhotoWalk *currentWalk = self.photoWalk;
+    NSLog(@"%@",currentWalk.name);
+    self.walkNameLabel.text = currentWalk.name;
+    
+    Location *firstLocation = currentWalk.locations[0];
+    Media *firstPhoto = firstLocation.photos[0];
+    [self.walkImageView setImageWithURL:[NSURL URLWithString:firstPhoto.url]];
+}
+
+@end
